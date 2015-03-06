3c9f0ea129be8907aa6491a3b068f4075f31cac8
Maksym Veremchuk <maksym.veremchuk@globallogic.com>
2015-03-06 17:38:23 +0200

    Link to posts

--- app/views.py
+++ app/views.py
@@ -7,12 +7,19 @@ from app import app
 def index():
     posts = [
         {
+            'id': 0,
             'title': "Title 1",
             'body': 'Beautiful day in Portland!'
         },
         {
+            'id': 1,
             'title': "Title 2",
             'body': 'The Avengers movie was so cool!'
         }
         ]
     return render_template("index.html", posts=posts)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    text = "This is post id: {}".format(post_id)
+    return render_template("post.html", title=text, text=text)
