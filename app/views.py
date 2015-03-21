4553be09a7fcede8dc61441f9918a756c0535ae6
Maksym Veremchuk <elysium31@gmail.com>
2015-03-21 14:02:39 +0200

    Attached to DB

--- app/views.py
+++ app/views.py
@@ -1,27 +1,28 @@
 from flask import render_template, g
-from app import app
+from app import app, db, models
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    posts = [
-        {
-            'id': 0,
-            'title': "Title 1",
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'id': 1,
-            'title': "Title 2",
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'id': 2,
-            'title': "Title 3",
-            'body': "Something interesting"
-        }
-        ]
+    # posts = [
+    #     {
+    #         'id': 0,
+    #         'title': "Title 1",
+    #         'body': 'Beautiful day in Portland!'
+    #     },
+    #     {
+    #         'id': 1,
+    #         'title': "Title 2",
+    #         'body': 'The Avengers movie was so cool!'
+    #     },
+    #     {
+    #         'id': 2,
+    #         'title': "Title 3",
+    #         'body': "Something interesting"
+    #     }
+    #     ]
+    posts = models.Post.query.all()
     return render_template("index.html", posts=posts)
 
 
@@ -32,5 +33,5 @@ def create_post():
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
-    text = "This is post id: {}".format(post_id)
-    return render_template("post.html", title=text, text=text)
+    post = models.Post.query.get(post_id)
+    return render_template("post.html", title=post.title, text=post.body)
